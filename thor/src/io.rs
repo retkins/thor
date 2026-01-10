1ef70f789ed900fac0b1f806a549d067ccbb1c5b
Ryan Etkins <retkins@pm.me>
2026-01-10 12:05:33 -0500

    Basic CSV parser for loading data sets - Added an error type for the project (to be expanded later) - Parser returns a simple struct with data in a flat vector (row-major format) and the number of rows and columns - Unit test confirms parser works with a simple file

---
+++ thor/src/io.rs
@@ -0,0 +1,110 @@
+use crate::errors::ThorError;
+use std::{fs::read_to_string, process::Termination};
+
+/// I/O routines 
+
+/// CSV data stored in row-major format
+pub struct CsvData {
+    _nrows: usize, 
+    _ncols: usize, 
+    _data: Vec<f64>
+}
+
+impl CsvData {
+    pub fn new() -> Self {
+        let default_capacity: usize = 1000; 
+        Self {
+            _nrows: 0, 
+            _ncols: 0, 
+            _data: Vec::with_capacity(default_capacity)
+        }
+    }
+    pub fn nrows(&self) -> usize {self._nrows}
+    pub fn ncols(&self) -> usize {self._ncols}
+    pub fn print(&self) {
+        println!("CSV Data Set of size {} x {}:\n", self.nrows(), self.ncols());
+        for i in 0..self.nrows() {
+            for j in 0..self.ncols() {
+                print!("{}", self._data[i*self.ncols()+j]);
+                if j < self.ncols() - 1 {
+                    print!(", ");
+                }
+
+            }
+            print!("\n");
+        }
+        println!("");
+    }
+}
+
+impl Termination for CsvData {
+    fn report(self) -> std::process::ExitCode {
+        std::process::ExitCode::SUCCESS
+    }
+}
+
+/// Read a csv file and return it as a matrix
+pub fn read_csv(filename: &str, delimiter: char, skiprows: usize) -> Result<CsvData, ThorError> {
+
+    let file_data: String = read_to_string(filename).map_err(|_| ThorError::FileOpenError)?;
+
+    let mut data: CsvData = CsvData::new(); 
+    let mut nrows: usize = 0;                   // number of rows of data
+    let mut ncols: usize;                       // number of columns of data/file
+    let mut line_number: usize = 0;             // line number of the file
+
+    // Read file line by line: 
+    // 1. Skip user-defined header rows 
+    // 2. Skip empty lines 
+    // 3. Split each line by delimiter and trim whitespace
+    // 4. Convert to f64 data
+    // 5. Check to make sure that each row has same number of columns
+    for line in file_data.lines() {
+        if line_number < skiprows {
+            line_number += 1;
+            continue;
+        }
+        if line.is_empty() {
+            line_number += 1; 
+            continue;
+        }
+
+        ncols = 0;
+        for word in line.split(delimiter) {
+            ncols += 1;
+            if nrows == 0 {
+                data._ncols += 1; 
+            }
+            let val: f64 = word.trim().parse::<f64>().map_err(|_| ThorError::FloatParseError)?;
+            data._data.push(val);
+        }
+        if data.ncols() != ncols {
+            return Err(ThorError::ColNumberChange);
+        }
+        line_number += 1;
+        nrows += 1;
+    }
+    data._nrows = nrows;
+
+    Ok(data)
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*; 
+    use crate::errors::ThorError;
+
+    /// Test loading a file containing standard delimiters and some
+    /// extra whitespace
+    #[test]
+    fn test_read_csv() -> Result<CsvData, ThorError>{
+        let filename: String = "tests/test_read.csv".to_owned(); 
+        let skiprows: usize = 1; 
+        let delimiter: char = ',';
+        let data: CsvData = read_csv(&filename, delimiter, skiprows)?; 
+        data.print();
+        Ok(data)
+    }
+}
